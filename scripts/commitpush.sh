65677cd280aeb0e6aed9aff09dbf90c106ca88f8
Pim Snel <post@pimsnel.com>
2022-01-06 22:26:13 +0100

    cleanup

--- scripts/commitpush.sh
+++
@@ -1,9 +0,0 @@
-if [[ ! -z "$(git diff --exit-code)" ]]; then
-    git add ./data/options.json
-    git commit -m "automatic update options.json"
-    git pull --rebase origin main
-    git push origin main
-else
-    echo "nothings changed will not commit anything"
-fi
-
