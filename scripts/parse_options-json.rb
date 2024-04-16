78f981b0a9a3f47f4ffc6f0dae1c4908302cce7c
mipmip <mipmip@users.noreply.github.com>
2024-04-16 19:59:15 +0000

    deploy: 12b2c51e7c839926621fd4963f5e98437ba4f9fc

--- scripts/parse_options-json.rb
+++ scripts/parse_options-json.rb
@@ -13,24 +13,38 @@ p ENV['HM_RELEASE']
 in_file = File.read("./result/share/doc/home-manager/options.json")
 parsed = JSON.parse(in_file)
 
+def isLiteralExpression(val, key)
+  if val.key? key and val[key].instance_of? Hash and val[key].key? "_type" and val[key]['_type'] == 'literalExpression'
+    true
+  else
+    false
+  end
+end
+
+def getValFor(val, key)
+  if isLiteralExpression(val, key)
+    val[key]['text']
+  elsif val.key? key
+    val[key]
+  else
+    ""
+  end
+end
+
+def parseVal(val)
+  val['example'] = getValFor(val, 'example')
+  val['default'] = getValFor(val, 'default')
+
+  val
+end
+
 options_arr = []
 parsed.each do | name, val |
 
   next if name == '_module.args'
 
   val['title'] = name
-
-  if val.key? "example"
-    val['example'] = val['example']['text']
-  else
-    val['example'] = ""
-  end
-
-  if val.key? "default"
-    val['default'] = val['default']['text']
-  else
-    val['default'] = ""
-  end
+  val = parseVal(val)
 
   options_arr << val
 end
