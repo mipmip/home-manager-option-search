224e59f2fff400138da554b97e5966ceec8e95c2
mipmip <mipmip@users.noreply.github.com>
2024-11-18 17:04:31 +0000

    deploy: a066932248b8a266906cf8fcabc2a40e01f645e8

--- js/script.js
+++ js/script.js
@@ -1,5 +1,131 @@
+
+/***********************************************
+ * MARKDOWN Parser for Builtins
+ */
+
+const TAGS = {
+  '': ['<em>','</em>'],
+  _: ['<strong>','</strong>'],
+  '*': ['<strong>','</strong>'],
+  '~': ['<s>','</s>'],
+  '\n': ['<br />'],
+  ' ': ['<br />'],
+  '-': ['<hr />']
+};
+
+/** Outdent a string based on the first indented line's leading whitespace
+ *	@private
+ */
+function outdent(str) {
+  return str.replace(RegExp('^'+(str.match(/^(\t| )+/) || '')[0], 'gm'), '');
+}
+
+/** Encode special attribute characters to HTML entities in a String.
+ *	@private
+ */
+function encodeAttr(str) {
+  return (str+'').replace(/"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+}
+
+/** Parse Markdown into an HTML String. */
+function parseMD(md, prevLinks) {
+  let tokenizer = /((?:^|\n+)(?:\n---+|\* \*(?: \*)+)\n)|(?:^``` *(\w*)\n([\s\S]*?)\n```$)|((?:(?:^|\n+)(?:\t|  {2,}).+)+\n*)|((?:(?:^|\n)([>*+-]|\d+\.)\s+.*)+)|(?:!\[([^\]]*?)\]\(([^)]+?)\))|(\[)|(\](?:\(([^)]+?)\))?)|(?:(?:^|\n+)([^\s].*)\n(-{3,}|={3,})(?:\n+|$))|(?:(?:^|\n+)(#{1,6})\s*(.+)(?:\n+|$))|(?:`([^`].*?)`)|(  \n\n*|\n{2,}|__|\*\*|[_*]|~~)/gm,
+    context = [],
+    out = '',
+    links = prevLinks || {},
+    last = 0,
+    chunk, prev, token, inner, t;
+
+  function tag(token) {
+    let desc = TAGS[token[1] || ''];
+    let end = context[context.length-1] == token;
+    if (!desc) return token;
+    if (!desc[1]) return desc[0];
+    if (end) context.pop();
+    else context.push(token);
+    return desc[end|0];
+  }
+
+  function flush() {
+    let str = '';
+    while (context.length) str += tag(context[context.length-1]);
+    return str;
+  }
+
+  md = md.replace(/^\[(.+?)\]:\s*(.+)$/gm, (s, name, url) => {
+    links[name.toLowerCase()] = url;
+    return '';
+  }).replace(/^\n+|\n+$/g, '');
+
+  while ( (token=tokenizer.exec(md)) ) {
+    prev = md.substring(last, token.index);
+    last = tokenizer.lastIndex;
+    chunk = token[0];
+    if (prev.match(/[^\\](\\\\)*\\$/)) {
+      // escaped
+    }
+    // Code/Indent blocks:
+    else if (t = (token[3] || token[4])) {
+      chunk = '<pre class="code '+(token[4]?'poetry':token[2].toLowerCase())+'"><code'+(token[2] ? ` class="language-${token[2].toLowerCase()}"` : '')+'>'+outdent(encodeAttr(t).replace(/^\n+|\n+$/g, ''))+'</code></pre>';
+    }
+    // > Quotes, -* lists:
+    else if (t = token[6]) {
+      if (t.match(/\./)) {
+        token[5] = token[5].replace(/^\d+/gm, '');
+      }
+      inner = parse(outdent(token[5].replace(/^\s*[>*+.-]/gm, '')));
+      if (t=='>') t = 'blockquote';
+      else {
+        t = t.match(/\./) ? 'ol' : 'ul';
+        inner = inner.replace(/^(.*)(\n|$)/gm, '<li>$1</li>');
+      }
+      chunk = '<'+t+'>' + inner + '</'+t+'>';
+    }
+    // Images:
+    else if (token[8]) {
+      chunk = `<img src="${encodeAttr(token[8])}" alt="${encodeAttr(token[7])}">`;
+    }
+    // Links:
+    else if (token[10]) {
+      out = out.replace('<a>', `<a href="${encodeAttr(token[11] || links[prev.toLowerCase()])}">`);
+      chunk = flush() + '</a>';
+    }
+    else if (token[9]) {
+      chunk = '<a>';
+    }
+    // Headings:
+    else if (token[12] || token[14]) {
+      t = 'h' + (token[14] ? token[14].length : (token[13]>'=' ? 1 : 2));
+      chunk = '<'+t+'>' + parse(token[12] || token[15], links) + '</'+t+'>';
+    }
+    // `code`:
+    else if (token[16]) {
+      chunk = '<code>'+encodeAttr(token[16])+'</code>';
+    }
+    // Inline formatting: *em*, **strong** & friends
+    else if (token[17] || token[1]) {
+      chunk = tag(token[17] || '--');
+    }
+    out += prev;
+    out += chunk;
+  }
+
+  return (out + md.substring(last) + flush()).replace(/^\n+|\n+$/g, '');
+}
+
+
+/*************************
+ * END MARKDOWN PARSER
+  */
+
+
 var search, results, allOptions, currentSet = [];
 var lastUpdate = "?";
+
+const useMarkdownParserEl = document.getElementById('useMarkdownParser');
+if (useMarkdownParserEl) {
+  var useMarkdownParser = useMarkdownParserEl.value
+}
 
 var indexOnDescriptionCheckbox = document.getElementById('indexOnDescriptionCheckbox');
 var indexOnTitleCheckbox = document.getElementById('indexOnTitleCheckbox');
@@ -11,6 +137,10 @@ var rebuildAndRerunSearch = function() {
   rebuildSearchIndex();
   searchOptions('');
 };
+
+
+
+
 
 var docOnload = function(){
 
@@ -78,7 +208,14 @@ var updateOptionsTable = function(options) {
     var tableRow = document.createElement('tr');
 
     var att = document.createAttribute("onClick");
-    att.value = "expandOption("+i+")";
+
+    if(useMarkdownParser ==="true") {
+      att.value = "expandOptionMD("+i+")";
+    }
+    else{
+      att.value = "expandOption("+i+")";
+    }
+
     tableRow.setAttributeNode(att);
 
     var att1 = document.createAttribute("class");
@@ -107,6 +244,16 @@ var updateOptionsTable = function(options) {
   }
 };
 
+
+
+
+
+
+
+
+
+
+
 function parseDescription(text){
 
   text = text.replace(/<https(\s*([^>]*))/gi ,'<a href="https$1">&lt;https$1</a>');
@@ -115,6 +262,26 @@ function parseDescription(text){
   text = text.replace(/\{var\}(\s*([^\n]*))/gi ,'<strong>$1</strong>').replace(/`/gi,'')
   text = text.replace(/:::\ \{\.note\}(\s*([^:::]*))/gi ,'<div class="alert alert-info" role="alert">$1</div>').replace(/:::/,'').replace(/\n/g, '<br />')
   return text;
+}
+
+var expandOptionMD = function(el){
+
+  modalTitle.innerHTML = currentSet[el].title;
+
+  let dhtml = parseMD(currentSet[el].doc);
+  var elDesc = "<h5 style='margin:1em 0 0 0'>Description</h5><div>" + dhtml  + "</div>";
+  var elArgs = "<h5 style='margin:1em 0 0 0'>Args</h5><div>" + currentSet[el].args.join(', ') + "</div>";
+  //  var elNote = ( currentSet[el].note == "" ? "": "<h5 style='margin:1em 0 0 0'>Note</h5><div>" + currentSet[el].note + "</div>");
+  //  var elDefault = "<h5 style='margin:1em 0 0 0'>Default</h5><div><pre style='margin-top:0.5em'>" + currentSet[el].default + "</pre></div>";
+  //  var elExample = ( currentSet[el].example == "" ? "" : "<h5 style='margin:1em 0 0 0'>Example</h5><div><pre style='margin-top:0.5em'>" + currentSet[el].example + "</pre></div>");
+  //
+  //  var declared_by_str = currentSet[el].declared_by;
+  //
+  //  var elDeclaredBy = "<h5 style='margin:1em 0 0 0'>Declared by</h5><div>" + declared_by_str+ "</div>";
+  //  modalBody.innerHTML = elDesc + elNote + elType + elDefault + elExample + elDeclaredBy;
+  modalBody.innerHTML = elDesc + elArgs;
+
+  $('#myModal').modal('show')
 }
 
 var expandOption = function(el){
